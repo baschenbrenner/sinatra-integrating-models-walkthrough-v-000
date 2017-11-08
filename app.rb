9742f6ec5a110a37964e6a2782ac5a3311d11f61
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-08 17:49:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,9 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-    
+    results = TextAnalyzer.new(text_from_user)
+    @count_of_words=results.count_of_words
+    @count_of_vowels=results.count_of_vowels
     erb :results
   end
 end
